2c4eb28e9bb0013b6e1aeef3b050bbbd4d94ed8d
Brian Cardarella <bcardarella@gmail.com>
2012-07-12 15:26:56 -0400

    ZOMG FASTER TEST SUITE!!!

--- template.rb
+++ template.rb
@@ -275,7 +275,7 @@ require 'database_cleaner'
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :deletion
   end
 
   config.before(:each) do
